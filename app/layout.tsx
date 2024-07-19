f55503b9de4d76b36f0356163f6894e535e6b2b3
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-19 11:46:15 -0300

    Creating the main containers

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "Get Coffe App",
-  description: "Get Coffe App",
+  title: 'Get Coffe App',
+  description: 'Get Coffe App',
 };
 
 export default function RootLayout({
@@ -15,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} bg-gray-100`}>{children}</body>
     </html>
   );
 }
