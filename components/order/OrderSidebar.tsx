d7a520337d4d04cbe3d5787552c4bb147afe350f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-19 19:15:27 -0300

    Data Fetching

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,15 @@
-function OrderSidebar() {
+import { prisma } from '@/src/lib/prisma';
+
+async function getCategories() {
+  const categories = await prisma.category.findMany();
+
+  return categories;
+}
+
+async function OrderSidebar() {
+  const categories = await getCategories();
+  console.log(categories);
+
   return <aside className='md:w-72 md:h-screen bg-white'>OrderSidebar</aside>;
 }
 
