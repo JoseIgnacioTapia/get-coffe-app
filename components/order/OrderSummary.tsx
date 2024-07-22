e9b45b0558fee928a429014ad01686607583c050
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-22 19:04:27 -0300

    Showing the items of the order

--- components/order/OrderSummary.tsx
+++ components/order/OrderSummary.tsx
@@ -1,5 +1,6 @@
-'use client'
+'use client';
 import { useStore } from '@/src/store';
+import ProductDetails from './ProductDetails';
 
 function OrderSummary() {
   const order = useStore((state) => state.order);
@@ -12,7 +13,9 @@ function OrderSummary() {
         <p className='text-center my-10'>El carrito esta vacío</p>
       ) : (
         <div className='mt-5'>
-          <p>Si hay algo</p>
+          {order.map((item) => (
+            <ProductDetails key={item.id} item={item} />
+          ))}
         </div>
       )}
     </aside>
