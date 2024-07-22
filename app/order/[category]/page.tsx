519b813f28c48727612701bced70a6d49bc4fbe1
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-22 16:31:52 -0300

    Iterating on Products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,3 +1,4 @@
+import ProductCard from '@/components/products/ProductCard';
 import { prisma } from '@/src/lib/prisma';
 
 async function getProducts(category: string) {
@@ -16,7 +17,13 @@ async function OrderPage({ params }: { params: { category: string } }) {
   const products = await getProducts(params.category);
   console.log(products);
 
-  return <div>OrderPage</div>;
+  return (
+    <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start'>
+      {products.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </div>
+  );
 }
 
 export default OrderPage;
--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -1,5 +1,21 @@
-function ProductCard() {
-  return <div>ProductCard</div>;
+import { formatCurrency } from '@/src/utils';
+import { Product } from '@prisma/client';
+
+type ProductCardProps = {
+  product: Product;
+};
+
+function ProductCard({ product }: ProductCardProps) {
+  return (
+    <div className='border bg-white'>
+      <div className='p-5'>
+        <h3 className='text-2xl font-bold'>{product.name}</h3>
+        <p className='mt-5 font-black text-4xl text-amber-500'>
+          {formatCurrency(product.price)}
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default ProductCard;
