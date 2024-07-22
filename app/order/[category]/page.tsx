61bb3c14c67793322cbe14a96a447e764c524e7f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-22 11:01:11 -0300

    Detect the current Category and get the Products

--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -1,5 +1,20 @@
-function OrderPage({ params }: { params: { category: string } }) {
-  console.log(params.category);
+import { prisma } from '@/src/lib/prisma';
+
+async function getProducts(category: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      category: {
+        slug: category,
+      },
+    },
+  });
+
+  return products;
+}
+
+async function OrderPage({ params }: { params: { category: string } }) {
+  const products = await getProducts(params.category);
+  console.log(products);
 
   return <div>OrderPage</div>;
 }
