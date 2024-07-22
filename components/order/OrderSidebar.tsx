57b69c7549d2322e4929c31869cb35b07cf92de3
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-22 09:45:59 -0300

    Iterating on the categories

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,4 +1,5 @@
 import { prisma } from '@/src/lib/prisma';
+import CategoryIcon from '../ui/CategoryIcon';
 
 async function getCategories() {
   const categories = await prisma.category.findMany();
@@ -10,7 +11,13 @@ async function OrderSidebar() {
   const categories = await getCategories();
   console.log(categories);
 
-  return <aside className='md:w-72 md:h-screen bg-white'>OrderSidebar</aside>;
+  return (
+    <aside className='md:w-72 md:h-screen bg-white'>
+      {categories.map((category) => (
+        <CategoryIcon key={category.id} category={category} />
+      ))}
+    </aside>
+  );
 }
 
 export default OrderSidebar;
