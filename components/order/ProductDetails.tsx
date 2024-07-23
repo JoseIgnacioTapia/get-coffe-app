ef390f8cb880769bdc771e615fc32a442e90118e
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-23 17:33:14 -0300

    Decrease the quantity of an item

--- components/order/ProductDetails.tsx
+++ components/order/ProductDetails.tsx
@@ -1,15 +1,30 @@
 import { XCircleIcon, MinusIcon, PlusIcon } from '@heroicons/react/24/outline';
+import { useStore } from '@/src/store';
+import { useMemo } from 'react';
 import { OrderItem } from '@/src/types';
 import { formatCurrency } from '@/src/utils';
-import { useStore } from '@/src/store';
 
 type ProductDetailsProps = {
   item: OrderItem;
 };
 
+const MIN_ITEMS = 1;
+const MAX_ITEMS = 5;
+
 function ProductDetails({ item }: ProductDetailsProps) {
   const increaseQuantity = useStore(
     (state: { increaseQuantity: any }) => state.increaseQuantity
+  );
+  const decreaseQuantity = useStore(
+    (state: { decreaseQuantity: any }) => state.decreaseQuantity
+  );
+  const disableDecreaseButton = useMemo(
+    () => item.quantity === MIN_ITEMS,
+    [item]
+  );
+  const disableIncreaseButton = useMemo(
+    () => item.quantity === MAX_ITEMS,
+    [item]
   );
 
   return (
@@ -26,13 +41,23 @@ function ProductDetails({ item }: ProductDetailsProps) {
           {formatCurrency(item.price)}
         </p>
         <div className='flex gap-5 px-10 py-2 bg-gray-100 w-fit rounded-lg'>
-          <button type='button' onClick={() => {}}>
+          <button
+            type='button'
+            onClick={() => decreaseQuantity(item.id)}
+            disabled={disableDecreaseButton}
+            className='disabled:opacity-20'
+          >
             <MinusIcon className='h-6 w-6' />
           </button>
 
           <p className='text-lg font-black '>{item.quantity}</p>
 
-          <button type='button' onClick={() => increaseQuantity(item.id)}>
+          <button
+            type='button'
+            onClick={() => increaseQuantity(item.id)}
+            disabled={disableIncreaseButton}
+            className='disabled:opacity-10'
+          >
             <PlusIcon className='h-6 w-6' />
           </button>
         </div>
