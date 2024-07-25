8b6bae8605ccdb53de9576f5f3dd90c54deb47b5
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-25 17:14:36 -0300

    Creating Logo, Routes and Layout to Dashboard

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,5 +1,6 @@
 import { prisma } from '@/src/lib/prisma';
 import CategoryIcon from '../ui/CategoryIcon';
+import Logo from '../ui/Logo';
 
 async function getCategories() {
   const categories = await prisma.category.findMany();
@@ -13,9 +14,12 @@ async function OrderSidebar() {
 
   return (
     <aside className='md:w-72 md:h-screen bg-white'>
-      {categories.map((category) => (
-        <CategoryIcon key={category.id} category={category} />
-      ))}
+      <Logo />
+      <nav className='mt-10'>
+        {categories.map((category) => (
+          <CategoryIcon key={category.id} category={category} />
+        ))}
+      </nav>
     </aside>
   );
 }
