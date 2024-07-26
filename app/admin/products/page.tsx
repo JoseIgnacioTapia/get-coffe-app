222c4bfe75aeee99764469affa655f070914ec19
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-26 11:01:33 -0300

    Navigation in the administration panel

---
+++ app/admin/products/page.tsx
@@ -0,0 +1,11 @@
+import Heading from '@/components/ui/Heading';
+
+function ProductsPage() {
+  return (
+    <>
+      <Heading>Administrar Productos</Heading>
+    </>
+  );
+}
+
+export default ProductsPage;
--- app/order/[category]/page.tsx
+++ app/order/[category]/page.tsx
@@ -21,9 +21,6 @@ async function OrderPage({ params }: { params: { category: string } }) {
   return (
     <>
       <Heading>Elige y personaliza tu pedido a continuación</Heading>
-      <h1 className='text-2xl my-10'>
-        Elige y personaliza tu pedido a continuación
-      </h1>
       <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4 items-start'>
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
