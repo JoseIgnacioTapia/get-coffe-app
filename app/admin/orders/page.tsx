2b311c9e9f4466a83bc50d47fe51571d95464fad
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-26 16:30:52 -0300

    Show Pending Orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+import OrderCard from '@/components/order/OrderCard';
 import Heading from '@/components/ui/Heading';
 import { prisma } from '@/src/lib/prisma';
 
@@ -7,7 +8,7 @@ async function getPendingOrders() {
       status: false,
     },
     include: {
-      OrderProducts: {
+      orderProducts: {
         include: {
           product: true,
         },
@@ -20,11 +21,20 @@ async function getPendingOrders() {
 
 async function OrdersPage() {
   const orders = await getPendingOrders();
-  console.log(JSON.stringify(orders, null, 2));
 
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
+
+      {orders.length ? (
+        <div className='grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-5 mt-5'>
+          {orders.map((order) => (
+            <OrderCard key={order.id} order={order} />
+          ))}
+        </div>
+      ) : (
+        <p className='text-center'>No hay ordenes pendientes</p>
+      )}
     </>
   );
 }
