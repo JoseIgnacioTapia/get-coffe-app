b7b169e68b88c73ee76a3c1274abe8872e446317
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-26 11:23:02 -0300

    Obtain Pending Orders

--- app/admin/orders/page.tsx
+++ app/admin/orders/page.tsx
@@ -1,6 +1,27 @@
 import Heading from '@/components/ui/Heading';
+import { prisma } from '@/src/lib/prisma';
 
-function OrdersPage() {
+async function getPendingOrders() {
+  const orders = await prisma.order.findMany({
+    where: {
+      status: false,
+    },
+    include: {
+      OrderProducts: {
+        include: {
+          product: true,
+        },
+      },
+    },
+  });
+
+  return orders;
+}
+
+async function OrdersPage() {
+  const orders = await getPendingOrders();
+  console.log(JSON.stringify(orders, null, 2));
+
   return (
     <>
       <Heading>Administrar Ordenes</Heading>
