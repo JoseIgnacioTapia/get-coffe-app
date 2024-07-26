b539586035c0b6f10c1b01ef89e1186d79a0047c
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-26 18:35:48 -0300

    Mark Orders Complete and Validation

--- actions/complete-order-action.ts
+++ actions/complete-order-action.ts
@@ -1,5 +1,25 @@
 'use server';
+import { prisma } from '@/src/lib/prisma';
+import { OrderIdSchema } from '@/src/schema';
 
-export async function completeOrder() {
-  console.log('desde completeOrder');
+export async function completeOrder(formData: FormData) {
+  const data = {
+    orderId: formData.get('order_id'),
+  };
+
+  const result = OrderIdSchema.safeParse(data);
+
+  try {
+    await prisma.order.update({
+      where: {
+        id: result.data.orderId,
+      },
+      data: {
+        status: true,
+        orderReadyAt: new Date(Date.now()),
+      },
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
