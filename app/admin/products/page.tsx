8478ff61bebdd30b3acdd0f02f514d55cf2b20b3
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-30 12:39:20 -0300

    Listing Products

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -1,9 +1,20 @@
 import Heading from '@/components/ui/Heading';
+import { prisma } from '@/src/lib/prisma';
+import ProductTable from '@/components/products/ProductsTable';
 
-function ProductsPage() {
+async function getProducts() {
+  const products = await prisma.product.findMany();
+  return products;
+}
+
+async function ProductsPage() {
+  const products = await getProducts();
+
   return (
     <>
       <Heading>Administrar Productos</Heading>
+
+      <ProductTable products={products} />
     </>
   );
 }
