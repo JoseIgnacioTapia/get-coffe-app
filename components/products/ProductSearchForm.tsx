847410fc7618e8d04c45c1484f0a71d3de07863f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-31 11:06:11 -0300

    Validating the Search Engine and redirect with useRouter

--- components/products/ProductSearchForm.tsx
+++ components/products/ProductSearchForm.tsx
@@ -1,6 +1,27 @@
+'use client';
+import { SearchSchema } from '@/src/schema';
+// import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+
 function ProductSearchForm() {
+  const router = useRouter();
+
+  const handleSearchForm = (formData: FormData) => {
+    const data = { search: formData.get('search') };
+
+    const result = SearchSchema.safeParse(data);
+    if (!result.success) {
+      result.error.issues.forEach((issue) => {
+        toast.error(issue.message);
+      });
+      return;
+    }
+    router.push(`/admin/products/search?search=${result.data.search}`);
+  };
+
   return (
-    <form className='flex items-center'>
+    <form action={handleSearchForm} className='flex items-center'>
       <input
         type='text'
         placeholder='Buscar Producto'
