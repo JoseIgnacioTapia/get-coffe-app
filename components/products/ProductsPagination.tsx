ed6d4cf87cd12425d998228013eb0c059415598f
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-30 21:20:28 -0300

    Add pager by numbers

--- components/products/ProductsPagination.tsx
+++ components/products/ProductsPagination.tsx
@@ -6,6 +6,9 @@ type ProductsPaginationProps = {
 };
 
 function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
+  const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+  console.log(pages);
+
   return (
     <nav className='flex justify-center py-10'>
       {page > 1 && (
@@ -16,6 +19,17 @@ function ProductsPagination({ page, totalPages }: ProductsPaginationProps) {
           &laquo;
         </Link>
       )}
+
+      {pages.map((currentPage) => (
+        <Link
+          className={`${
+            page === currentPage && 'bg-gray-700 text-white'
+          } bg-white px-4 py-2 text-sm text-gray-900 ring-1 ring-inset ring-gray-300 focus:z-20 focus:outline-offset-0`}
+          href={`/admin/products?page=${currentPage}`}
+        >
+          {currentPage}
+        </Link>
+      ))}
 
       {page < totalPages && (
         <Link
