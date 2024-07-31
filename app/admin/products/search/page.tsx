98da77ffe77f54e233c27d53d07fa9c2172508f7
JoseIgnacioTapia <josignaciot@gmail.com>
2024-07-31 11:43:59 -0300

    Showing search results

--- app/admin/products/search/page.tsx
+++ app/admin/products/search/page.tsx
@@ -1,13 +1,46 @@
 import Heading from '@/components/ui/Heading';
+import ProductTable from '@/components/products/ProductsTable';
+import { prisma } from '@/src/lib/prisma';
+import ProductSearchForm from '@/components/products/ProductSearchForm';
+
+async function searchProducts(searchTerm: string) {
+  const products = await prisma.product.findMany({
+    where: {
+      name: {
+        contains: searchTerm,
+        mode: 'insensitive',
+      },
+    },
+    include: {
+      category: true,
+    },
+  });
+
+  return products;
+}
 
 async function SearchPage({
   searchParams,
 }: {
   searchParams: { search: string };
 }) {
-  console.log(searchParams.search);
+  const products = await searchProducts(searchParams.search);
 
-  return <Heading>Página de búsqueda</Heading>;
+  return (
+    <>
+      <Heading>Página de búsqueda: {searchParams.search}</Heading>
+
+      <div className='flex flex-col lg:flex-row lg:justify-end gap-5'>
+        <ProductSearchForm />
+      </div>
+
+      {products.length ? (
+        <ProductTable products={products} />
+      ) : (
+        <p className='text-center text-lg'>No hay resultados</p>
+      )}
+    </>
+  );
 }
 
 export default SearchPage;
