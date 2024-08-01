749814c2b1aba054419fc92f74293ba08d954f20
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-01 20:18:28 -0300

    Get the URL of the image being uploaded

--- components/products/ImageUpload.tsx
+++ components/products/ImageUpload.tsx
@@ -1,12 +1,20 @@
 'use client';
+import Image from 'next/image';
 import { CldUploadWidget } from 'next-cloudinary';
+import { useState } from 'react';
 import { TbPhotoPlus } from 'react-icons/tb';
 
 function ImageUpload() {
+  const [imageUrl, setImageUrl] = useState('');
+
   return (
     <CldUploadWidget
       onSuccess={(result, { widget }) => {
-        console.log(result);
+        if (result.event === 'success') {
+          widget.close();
+          // @ts-ignore
+          setImageUrl(result.info?.secure_url);
+        }
       }}
       uploadPreset='qemty2dl'
       options={{ maxFiles: 1 }}
@@ -21,8 +29,21 @@ function ImageUpload() {
             >
               <TbPhotoPlus size={50} />
               <p className='text-lg font-semibold'>Agregar Imagen</p>
+
+              {imageUrl && (
+                <div className='absolute inset-0 w-full h-full'>
+                  <Image
+                    fill
+                    style={{ objectFit: 'contain' }}
+                    src={imageUrl}
+                    alt='Imagen de producto'
+                  />
+                </div>
+              )}
             </div>
           </div>
+
+          <input type='hidden' name='image' value={imageUrl} />
         </>
       )}
     </CldUploadWidget>
