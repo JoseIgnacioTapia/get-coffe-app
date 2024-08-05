703e6fcc4a853fdee80e901b3fc2967099861aca
JoseIgnacioTapia <josignaciot@gmail.com>
2024-08-05 16:34:56 -0300

    Creating the API Endpoint and List Order Screen

---
+++ app/orders/page.tsx
@@ -0,0 +1,13 @@
+import Logo from '@/components/ui/Logo';
+
+function OrdersPage() {
+  return (
+    <>
+      <h1 className='text-center mt-20 text-6xl font-black'>Oredenes Listas</h1>
+
+      <Logo />
+    </>
+  );
+}
+
+export default OrdersPage;
